c656b4e094b86eb15596bbbdd2546f3fca73dc8d
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-18 20:23:51 +0800

    Update __init__.py

--- threesome/__init__.py
+++ threesome/__init__.py
@@ -2,17 +2,20 @@
 # -*- coding: utf-8 -*-
 """
 =============================================================================
-MYLOVE ULTIMATE VERSI 1 - THREESOME PACKAGE INIT
+MYLOVE ULTIMATE VERSI 1 - THREESOME PACKAGE INIT (FIX)
 =============================================================================
 Mode threesome dengan 2 HTS, 2 FWB, atau kombinasi HTS+FWB
+FIX: Menambahkan absolute imports
 """
 
-from .manager import ThreesomeManager
-from .dynamics import ThreesomeDynamics
+from threesome.manager import ThreesomeManager, ThreesomeType, ThreesomeStatus
+from threesome.dynamics import ThreesomeDynamics
 
 __all__ = [
     'ThreesomeManager',
     'ThreesomeDynamics',
+    'ThreesomeType',
+    'ThreesomeStatus',
 ]
 
 # Version info
