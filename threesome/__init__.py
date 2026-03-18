e28cfe050a0805e98869c7a110d8ed379a750086
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-18 16:15:30 +0800

    Create __init__.py

---
+++ threesome/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+=============================================================================
+MYLOVE ULTIMATE VERSI 1 - THREESOME PACKAGE INIT
+=============================================================================
+Mode threesome dengan 2 HTS, 2 FWB, atau kombinasi HTS+FWB
+"""
+
+from .manager import ThreesomeManager
+from .dynamics import ThreesomeDynamics
+
+__all__ = [
+    'ThreesomeManager',
+    'ThreesomeDynamics',
+]
+
+# Version info
+__version__ = "1.0.0"
